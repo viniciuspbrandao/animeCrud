742300e56868e63b53e10e58f8a696f90ff4f734
viniciuspbrandao <brandao.vinicius@outlook.com>
2025-06-17 17:20:13 -0300

    refactor: domain entity

--- src/main/java/com/animes/animescrud/domain/domain/Anime.java
+++ src/main/java/com/animes/animescrud/domain/domain/Anime.java
@@ -1,20 +1,7 @@
 package com.animes.animescrud.domain.domain;
 
-
 import com.animes.animescrud.domain.enums.TipoGenero;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
-@Getter
-@Setter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
 public class Anime {
 
     private Long id;
@@ -25,4 +12,85 @@ public class Anime {
     private String synopsis;
     private TipoGenero tipoGenero;
 
+    public Anime() {
+    }
+
+    public Anime(Long id, String name, String author, int yearPublication, int episodesNumber, String synopsis, TipoGenero tipoGenero) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.yearPublication = yearPublication;
+        this.episodesNumber = episodesNumber;
+        this.synopsis = synopsis;
+        this.tipoGenero = tipoGenero;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYearPublication() {
+        return yearPublication;
+    }
+
+    public void setYearPublication(int yearPublication) {
+        this.yearPublication = yearPublication;
+    }
+
+    public int getEpisodesNumber() {
+        return episodesNumber;
+    }
+
+    public void setEpisodesNumber(int episodesNumber) {
+        this.episodesNumber = episodesNumber;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public TipoGenero getTipoGenero() {
+        return tipoGenero;
+    }
+
+    public void setTipoGenero(TipoGenero tipoGenero) {
+        this.tipoGenero = tipoGenero;
+    }
+
+    @Override
+    public String toString() {
+        return "Anime{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", yearPublication=" + yearPublication +
+                ", episodesNumber=" + episodesNumber +
+                ", synopsis='" + synopsis + '\'' +
+                ", tipoGenero=" + tipoGenero +
+                '}';
+    }
 }
